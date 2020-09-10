9950a33765d979cb8151d18032b4ecc0286cd454
rmjuarez12 <rmjuarez12@gmail.com>
2020-09-10 02:15:14 -0400

    Finished with MVP tasks

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,15 @@
+// Import Axios
+import axios from "axios";
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+const gitUserName = "rmjuarez12";
+const APIUrl = `https://api.github.com/users/${gitUserName}`;
+const getGitData = axios.get(APIUrl);
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -12,10 +19,24 @@
     Skip to STEP 3.
 */
 
+getGitData.then((res) => {
+  // console.log to inspect data returned
+  console.log("Data returned", res);
+});
+
 /*
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+
+getGitData
+  .then((res) => {
+    const cardParent = document.querySelector(".cards");
+    const cardMakerFunction = cardMaker(res.data);
+
+    cardParent.prepend(cardMakerFunction);
+  })
+  .catch((err) => console.log(err));
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -28,7 +49,32 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+// Get the followers data
+const APIUrlFollowers = `https://api.github.com/users/${gitUserName}/followers`;
+const getFollowersData = axios.get(APIUrlFollowers);
+
+getFollowersData
+  .then((res) => {
+    // Save all user data in a variable
+    const followersArray = res.data;
+
+    // Get the parent div to append to
+    const cardParent = document.querySelector(".cards");
+
+    // Add a title separator
+    const followersTitle = document.createElement("h2");
+    followersTitle.textContent = "Followers";
+    followersTitle.classList.add("followers-heading");
+    cardParent.appendChild(followersTitle);
+
+    // Insert the followers in the DOM
+    followersArray.forEach((user) => {
+      const cardMakerFunction = cardMaker(user);
+
+      cardParent.appendChild(cardMakerFunction);
+    });
+  })
+  .catch((err) => console.log(err));
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -49,6 +95,78 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function cardMaker(user) {
+  // Create the main container
+  const cardContainer = document.createElement("div");
+
+  // Add attributes to main container
+  cardContainer.classList.add("card");
+
+  // Create the image element
+  const userImg = document.createElement("img");
+
+  // Add source image to the image element
+  userImg.src = user.avatar_url;
+
+  // Create the elements for the card info
+  const cardInfoContainer = document.createElement("div");
+  const cardName = document.createElement("h3");
+  const cardUserName = document.createElement("p");
+  const cardLocation = document.createElement("p");
+  const cardProfile = document.createElement("p");
+  const cardFollowers = document.createElement("p");
+  const cardFollowing = document.createElement("p");
+  const cardBio = document.createElement("p");
+
+  // Add attributes to card info elements
+  cardInfoContainer.classList.add("card-info");
+  cardName.classList.add("name");
+  cardUserName.classList.add("username");
+
+  // Add content to card-info elements
+  cardName.textContent = user.name;
+  cardUserName.textContent = user.login;
+  cardLocation.textContent = `Location: ${user.location}`;
+  cardProfile.textContent = "Profile: ";
+  cardFollowers.textContent = `Followers: ${user.followers}`;
+  cardFollowing.textContent = `Following: ${user.following}`;
+  cardBio.textContent = `Bio: ${user.bio}`;
+
+  // Create xtra inner HTML elements for certain pieces of info on the card info elements
+  const cardProfileURL = document.createElement("a");
+
+  // Add content to the profile link element and insert into profile element
+  cardProfileURL.textContent = user.html_url;
+  cardProfileURL.setAttribute("href", user.html_url);
+  cardProfileURL.setAttribute("target", "_blank");
+  cardProfile.appendChild(cardProfileURL);
+
+  // Insert all necessary childs into the card info
+  const cardInfoChildren = [
+    cardName,
+    cardUserName,
+    cardLocation,
+    cardProfile,
+    cardFollowers,
+    cardFollowing,
+    cardBio,
+  ];
+
+  cardInfoChildren.forEach((element) => {
+    cardInfoContainer.appendChild(element);
+  });
+
+  // Insert all necessary childs into the main card container
+  const mainCardChildren = [userImg, cardInfoContainer];
+
+  mainCardChildren.forEach((element) => {
+    cardContainer.appendChild(element);
+  });
+
+  // Return the entire card element
+  return cardContainer;
+}
 
 /*
   List of LS Instructors Github username's:
